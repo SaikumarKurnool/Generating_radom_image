64717526e9529f88c53bd2e06777d8b80e63398d
SaikumarKurnool <60703864+SaikumarKurnool@users.noreply.github.com>
2020-02-05 20:43:30 +0530

    Add files via upload

---
+++ random_image_generation.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+'''
+   This program generates a random image of dimensions 256x512 and saves it in current folder
+   (for keeping it simple we are going with a single channel image i.e., black and white image)
+'''
+
+height = 256
+width = 512
+
+#generating random numpy array which ranges from 0 to 255
+img = (np.random.rand(height, width)*255).astype('uint8')
+
+#saving generated image
+cv2.imwrite('./out.png', img)
+
+
